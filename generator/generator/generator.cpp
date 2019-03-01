df0274a5eb439063125b6a322f3b9ae55c264514
Pedro Moreira <pedrombm19@gmail.com>
2019-03-01 12:02:45 +0000

    Update generator.cpp

--- generator/generator/generator.cpp
+++ generator/generator/generator.cpp
@@ -28,15 +28,78 @@ int fileWriter(string file, string content) {
   return 0;
 }
 
-void planeHandler(char* size, char* destFile){
-	string s = string("Guardar quadrado com lado ")+ size +", no ficheiro "+ destFile +"\n";
+void planeHandler(int size, char* destFile){
+
+	//string s = string("Guardar quadrado com lado ") + std::to_string(size) + ", no ficheiro " + destFile + "\n";
+	string s = "<" + to_string(size/2) + ", 0," + to_string(-size/2) + ",\n" +
+					 to_string(-size/2) + ", 0," + to_string(-size/2) + ",\n" + 
+					 to_string(size/2) + ", 0," + to_string(size/2) + ">\n" + 
+			   "<" + to_string(-size/2) + ", 0," + to_string(-size/2) + ",\n" +
+					 to_string(-size/2) + ", 0," + to_string(size/2) + ",\n" +
+					 to_string(size/2) + ", 0," + to_string(size/2) + ">";
+
 	fileWriter(destFile, s);
 }
 
-void boxHandler(char* x, char* y, char* z, char* d, char* destFile){
+void boxHandler(int x, int y, int z, int d, char* destFile){
 	string s;
-	if(!d) s = string("Guardar caixa com x ")+ x +", y "+ y +" e z "+ z +" no ficheiro "+ destFile +" \n";
-	else s = string("Guardar caixa com x ") + x +", y "+ y +", z "+ z +" e "+ d +" dimensoes  no ficheiro "+ destFile +" \n";
+	string x_str = to_string(x);
+	string y_str = to_string(y);
+	string z_str = to_string(z);
+	float div = y / d;
+	float height = 0;
+
+	// base
+	s += "<0, 0, " + z_str + ",\n" +
+		 x_str + ", 0, 0,\n" +
+		 x_str + ", 0, " + z_str + ">\n" +		 
+		"<0, 0, " + z_str + ",\n" + 
+		"0, 0, 0,\n" +
+		x_str + ", 0, 0>\n";
+
+	// faces
+	for (int i = 0; i < d; i++, height += div) {
+		string h_str = to_string(height);
+		string h2_str = to_string(height + div);
+
+		// frontal
+		s += "<triangle>\n\t<point X=0 Y=" + h_str + " Z=" + z_str + "\>\n" +
+			"\t<point X=" + x_str + " Y=" + h_str + " Z=" + z_str + "\>\n" +
+			"\t<point X=" + x_str + " Y=" + h2_str + " Z=" + z_str + "\>\n" + "</triangle>\n" +
+			"<triangle>\n\t<point X=" + x_str + " Y=" + h2_str + " Z=" + z_str + "\>\n" +
+			"\t<point X=0 Y=" + h2_str + " Z=" + z_str + "\>\n" +
+			"\t<point X=0 Y=" + h_str + " Z=" + z_str + "\>\n" + "</triangle>\n";
+			
+
+		// lateral visivel
+		s += "<" + x_str + ", " + h_str + ", " + z_str + ",\n" +
+			x_str + ", " + h_str + ", 0,\n" +
+			x_str + ", " + h2_str + ", 0>\n" +
+			"<" + x_str + ", " + h2_str + ", 0,\n" +
+			x_str + ", " + h2_str + ", " + z_str + ",\n" +
+			x_str + ", " + h_str + ", " + z_str + ">\n";
+
+		// lateral invisivel
+
+		// traseira
+	}
+
+	// topo
+	s += "<0, " + y_str + ", " + z_str + ",\n" +
+		x_str + ", " + y_str + ", " + z_str + ",\n" +
+		x_str + ", " + y_str + ", 0>\n" +
+		"<0, " + y_str + ", " + z_str + ",\n" +
+		x_str + ", " + y_str + ", 0,\n" +
+		"0, " + y_str + ", 0>\n";
+	/*
+	if (d == 0) {
+		s = string("Guardar caixa com x ") + x + ", y " + y + " e z " + z + " no ficheiro " + destFile + " \n";
+	} 
+	else {
+		s = string("Guardar caixa com x ") + x + ", y " + y + ", z " + z + " e " + d + " dimensoes  no ficheiro " + destFile + " \n";
+	}
+	*/
+
 	fileWriter(destFile, s);
 }
 
@@ -57,17 +120,20 @@ int main(int argc, char** argv){
 
 	switch (hashF(argv[1])){
     	case PLANE:
-        	if(argc == 4) planeHandler(argv[2],argv[3]);
+        	if(argc == 4) planeHandler(atoi(argv[2]), argv[3]);
         	else error_flag = 1;
         	break;
 
     	case BOX:
         	if(argc >= 6){
-        		char* dim = NULL;
+        		int dim = 1;
 				char* fdest = argv[5];
-				if(argc == 7) { dim = argv[5]; fdest = argv[6];}
-
-				boxHandler(argv[2],argv[3],argv[4],dim,fdest);
+				
+				if(argc == 7) { 
+					dim = atoi(argv[5]); 
+					fdest = argv[6];
+				}
+				boxHandler(atoi(argv[2]), atoi(argv[3]), atoi(argv[4]), dim, fdest);
         	}
         	else error_flag = 1;
         	break;
@@ -83,11 +149,11 @@ int main(int argc, char** argv){
         	break;
 
     	default:
-       		printf("Por favor, insira um sólido válido.\n");
+       		printf("Por favor, insira um sólido válido. \n");
         	break;
     }
 
-    if(error_flag) printf("Por favor insira todos os parâmetros necessários\n");
+    if(error_flag) printf("Por favor insira todos os parâmetros necessários. \n");
 
 	return 1;
 
