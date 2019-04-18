d0d4fd64208dc899f624dd87171e6079c4029142
PedroFerr101 <pferreira59@gmail.com>
2019-04-18 10:46:38 +0100

    add missing files

---
+++ src/common/headers/matrixOperations.h
@@ -0,0 +1,15 @@
+#ifndef MATRIX_OP
+#define MATRIX_OP
+
+#include <math.h>
+
+void cross(float *a, float *b, float *res);
+void normalize(float *a);
+float length(float *v);
+void multMatrixVector(float *m, float *v, float *res);
+void multVectorMatrix(float *v, float *m, float *res);
+void multMatrixMatrix(float *m1, float *m2, float *res);
+void buildRotTransMatrix(float *x, float *y, float *z, float *pos, float *m);
+void multLVectorCVector(float *lv, float *cv, float *res);
+
+#endif
---
+++ src/common/matrixOperations.cpp
@@ -0,0 +1,95 @@
+#include <matrixOperations.h>
+
+void cross(float *a, float *b, float *res) {
+
+	res[0] = a[1] * b[2] - a[2] * b[1];
+	res[1] = a[2] * b[0] - a[0] * b[2];
+	res[2] = a[0] * b[1] - a[1] * b[0];
+}
+
+
+void normalize(float *a) {
+
+	float l = sqrt(a[0] * a[0] + a[1] * a[1] + a[2] * a[2]);
+	a[0] = a[0] / l;
+	a[1] = a[1] / l;
+	a[2] = a[2] / l;
+}
+
+
+float length(float *v) {
+
+	float res = sqrt(v[0] * v[0] + v[1] * v[1] + v[2] * v[2]);
+	return res;
+
+}
+
+void multMatrixVector(float *m, float *v, float *res) {
+
+	for (int j = 0; j < 4; ++j) {
+		res[j] = 0;
+		for (int k = 0; k < 4; ++k) {
+			res[j] += v[k] * m[j * 4 + k];
+		}
+	}
+
+}
+
+void multVectorMatrix(float *v, float *m, float *res) {
+	for (int i = 0; i < 4; ++i) {
+		res[i] = 0;
+		for (int j = 0; j < 4; ++j) {
+			res[i] += v[j] * m[j * 4 + i];
+		}
+	}
+}
+
+void multMatrixMatrix(float *m1, float *m2, float *res) {
+	for (int i = 0; i < 4; ++i) {
+		for (int j = 0; j < 4; ++j) {
+			res[i * 4 + j] = 0.0f;
+			for (int k = 0; k < 4; ++k)
+				res[i * 4 + j] += m1[i * 4 + k] * m2[k * 4 + j];
+		}
+	}
+}
+
+void buildRotTransMatrix(float *x, float *y, float *z, float *pos, float *m) {
+	// matriz transposta 
+	m[0] = x[0]; m[1] = x[1]; m[2] = x[2]; m[3] = 0;
+	m[4] = y[0]; m[5] = y[1]; m[6] = y[2]; m[7] = 0;
+	m[8] = z[0]; m[9] = z[1]; m[10] = z[2]; m[11] = 0;
+	m[12] = pos[0]; m[13] = pos[1]; m[14] = pos[2]; m[15] = 1; // ao usar o ponto calculado podemos aplicar a translacao atraves da matriz
+}
+
+
+
+
+void multLVectorCVector(float *lv, float *cv, float *res) {
+	(*res) = 0;
+	for (int j = 0; j < 4; ++j) {
+		(*res) += cv[j] * lv[j];
+	}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
